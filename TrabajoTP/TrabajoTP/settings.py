7d17a61b5f2deb7e0d259d1a7fe4fccd96d7d333
Gonzalo Agustin  Romero Jensen <gonzaloromeroj@gmail.com>
2025-10-10 19:57:59 -0300

    Tranca Prueba 2

--- TrabajoTP/TrabajoTP/settings.py
+++ TrabajoTP/TrabajoTP/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'views.py',
-    'forms.py',
-    'models.py'
 ]
 
 MIDDLEWARE = [
